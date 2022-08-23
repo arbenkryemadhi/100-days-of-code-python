28b9ef4008c1d64138f739e9dda434fd33c2ab9c
arbenkryemadhi <arbenofc@gmail.com>
2022-08-23 17:30:09 +0200

    Update main.py

--- Exercises/01 Band Name Generator/main.py
+++ Exercises/01 Band Name Generator/main.py
@@ -1,4 +1,4 @@
-print("Hello, Welcome to the Band Name Generator. Make by Arben KRYEMADHI!")
+print("Hello, Welcome to the Band Name Generator. Made by Arben KRYEMADHI!")
 city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("What's the name of your pet?\n")
 print(f"Your band make could be {city_name} {pet_name}!")
